9a71a8aa7778973158a2206e3b6eb164cadaac86
Kennedy Ho <kennedyho@KennedyHoMacBook.local>
2020-02-22 15:09:54 +0900

    Contains Duplicate implementation

---
+++ Arrays/src/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+/* Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array,
+and it should return false if every element is distinct. */
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        // Use a hashset to obtain O(n) time complexity
+        // Accessing an item in the hashset is O(1) time complexity
+        // However, more memory will be used to maintain another set of N numbers.
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
